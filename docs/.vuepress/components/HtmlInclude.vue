d09d2ec4c9991ab268e67d766999918fa1e87a88
dante7qx <sunchao.0129@163.comm>
2025-06-03 14:42:27 +0800

    添加MCP协议文档

---
+++ docs/.vuepress/components/HtmlInclude.vue
@@ -0,0 +1,18 @@
+<template>
+  <div ref="container"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: { type: String, required: true },
+  },
+  mounted() {
+    fetch(this.src)
+        .then((res) => res.text())
+        .then((html) => {
+          this.$refs.container.innerHTML = html;
+        });
+  },
+};
+</script>
